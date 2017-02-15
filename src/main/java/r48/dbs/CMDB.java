8818b319ac26f234860704f1251e59bfcd098b3f
20kdc <asdd2808@gmail.com>
2017-02-15 13:12:34 +0000

    Add a new way to specify multiple-type parameters
    This is less flexible than using custom schemas, but should allow for some of the cases where it would otherwise be not worth bothering to fix to be fixed.

--- src/main/java/r48/dbs/CMDB.java
+++ src/main/java/r48/dbs/CMDB.java
@@ -5,6 +5,7 @@
 
 package r48.dbs;
 
+import gabien.ui.IFunction;
 import r48.AppMain;
 import r48.RubyIO;
 import r48.schema.EnumSchemaElement;
@@ -39,10 +40,40 @@ public class CMDB {
                 if (c == 'p') {
                     rc.paramName.add(args[0].trim());
                     String s = args[1].trim();
-                    ISchemaElement se = localAliasing.get(s);
-                    if (se == null)
-                        se = AppMain.schemas.getSDBEntry(s);
-                    rc.paramType.add(se);
+                    final ISchemaElement se = aliasingAwareSG(s);
+                    rc.paramType.add(new IFunction<RubyIO, ISchemaElement>() {
+                        @Override
+                        public ISchemaElement apply(RubyIO rubyIO) {
+                            return se;
+                        }
+                    });
+                }
+                if (c == 'D') {
+                    rc.paramName.add(args[0].trim());
+                    final int arrayDI = Integer.parseInt(args[1]);
+                    final ISchemaElement defaultSE = aliasingAwareSG(args[2]);
+                    final HashMap<Integer, ISchemaElement> h = new HashMap<Integer, ISchemaElement>();
+                    for (int i = 3; i < args.length; i += 2) {
+                        int ind = Integer.parseInt(args[i]);
+                        ISchemaElement se = aliasingAwareSG(args[i + 1]);
+                        h.put(ind, se);
+                    }
+                    rc.paramType.add(new IFunction<RubyIO, ISchemaElement>() {
+                        @Override
+                        public ISchemaElement apply(RubyIO rubyIO) {
+                            if (rubyIO == null)
+                                return defaultSE;
+                            if (rubyIO.arrVal == null)
+                                return defaultSE;
+                            if (rubyIO.arrVal.length <= arrayDI)
+                                return defaultSE;
+                            int p = (int) rubyIO.arrVal[arrayDI].fixnumVal;
+                            ISchemaElement ise = h.get(p);
+                            if (ise != null)
+                                return ise;
+                            return defaultSE;
+                        }
+                    });
                 }
                 if (c == 'i')
                     rc.indentPre = Integer.parseInt(args[0]);
@@ -60,6 +91,13 @@ public class CMDB {
                     localAliasing.put(args[0], AppMain.schemas.getSDBEntry(args[1]));
                 if (c == 'X')
                     rc.specialSchemaName = args[0];
+            }
+
+            private ISchemaElement aliasingAwareSG(String s) {
+                ISchemaElement se = localAliasing.get(s);
+                if (se == null)
+                    se = AppMain.schemas.getSDBEntry(s);
+                return se;
             }
         });
     }
