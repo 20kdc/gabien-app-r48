da42d2ca846c854f41ac44dcc7f04932f7fc94da
20kdc <asdd2808@gmail.com>
2017-06-06 23:48:22 +0100

    R2k backend: Added Enemy!

--- src/main/java/r48/io/r2k/obj/ldb/Enemy.java
+++ src/main/java/r48/io/r2k/obj/ldb/Enemy.java
@@ -4,17 +4,83 @@
  */
 package r48.io.r2k.obj.ldb;
 
+import gabien.ui.ISupplier;
 import r48.RubyIO;
 import r48.io.r2k.Index;
-import r48.io.r2k.chunks.R2kObject;
+import r48.io.r2k.chunks.*;
 
 /**
  * COPY jun6-2017
  */
 public class Enemy extends R2kObject {
+    public StringR2kStruct name = new StringR2kStruct();
+    public StringR2kStruct battlerName = new StringR2kStruct();
+    public IntegerR2kStruct battlerHue = new IntegerR2kStruct(0);
+    public IntegerR2kStruct maxHp = new IntegerR2kStruct(10);
+    public IntegerR2kStruct maxSp = new IntegerR2kStruct(10);
+    public IntegerR2kStruct atk = new IntegerR2kStruct(10);
+    public IntegerR2kStruct def = new IntegerR2kStruct(10);
+    public IntegerR2kStruct spi = new IntegerR2kStruct(10);
+    public IntegerR2kStruct agi = new IntegerR2kStruct(10);
+    public BooleanR2kStruct battlerBlendMode = new BooleanR2kStruct(false);
+    public IntegerR2kStruct exp = new IntegerR2kStruct(0);
+    public IntegerR2kStruct gold = new IntegerR2kStruct(0);
+    public IntegerR2kStruct dropId = new IntegerR2kStruct(0);
+    public IntegerR2kStruct dropProb = new IntegerR2kStruct(100);
+    public BooleanR2kStruct criticalHit = new BooleanR2kStruct(false);
+    public IntegerR2kStruct criticalHitChance = new IntegerR2kStruct(30);
+    public BooleanR2kStruct miss = new BooleanR2kStruct(false);
+    public BooleanR2kStruct levitate = new BooleanR2kStruct(false);
+
+    public ArraySizeR2kInterpretable<ByteR2kStruct> stateRanksSz = new ArraySizeR2kInterpretable<ByteR2kStruct>();
+    public ArrayR2kStruct<ByteR2kStruct> stateRanks = new ArrayR2kStruct<ByteR2kStruct>(stateRanksSz, new ISupplier<ByteR2kStruct>() {
+        @Override
+        public ByteR2kStruct get() {
+            return new ByteR2kStruct(0);
+        }
+    }, true);
+    public ArraySizeR2kInterpretable<ByteR2kStruct> attrRanksSz = new ArraySizeR2kInterpretable<ByteR2kStruct>();
+    public ArrayR2kStruct<ByteR2kStruct> attrRanks = new ArrayR2kStruct<ByteR2kStruct>(attrRanksSz, new ISupplier<ByteR2kStruct>() {
+        @Override
+        public ByteR2kStruct get() {
+            return new ByteR2kStruct(0);
+        }
+    }, true);
+
+    public SparseArrayAR2kStruct<EnemyAction> enemyActions = new SparseArrayAR2kStruct<EnemyAction>(new ISupplier<EnemyAction>() {
+        @Override
+        public EnemyAction get() {
+            return new EnemyAction();
+        }
+    });
+
     @Override
     public Index[] getIndices() {
-        return new Index[0];
+        return new Index[] {
+                new Index(0x01, name, "@name"),
+                new Index(0x02, battlerName, "@battler_name"),
+                new Index(0x03, battlerHue, "@battler_hue"),
+                new Index(0x04, maxHp, "@max_hp"),
+                new Index(0x05, maxSp, "@max_sp"),
+                new Index(0x06, atk, "@atk"),
+                new Index(0x07, def, "@def"),
+                new Index(0x08, spi, "@spi"),
+                new Index(0x09, agi, "@agi"),
+                new Index(0x0A, battlerBlendMode, "@battler_blend_mode"),
+                new Index(0x0B, exp, "@exp"),
+                new Index(0x0C, gold, "@gold"),
+                new Index(0x0D, dropId, "@drop_item"),
+                new Index(0x0E, dropProb, "@drop_percent"),
+                new Index(0x15, criticalHit, "@can_crit"),
+                new Index(0x16, criticalHitChance, "@crit_chance"),
+                new Index(0x1A, miss, "@miss"),
+                new Index(0x1C, levitate, "@levitate"),
+                new Index(0x1F, stateRanksSz),
+                new Index(0x20, stateRanks, "@state_ranks"),
+                new Index(0x21, attrRanksSz),
+                new Index(0x22, attrRanks, "@attribute_ranks"),
+                new Index(0x2A, enemyActions, "@actions"),
+        };
     }
 
     @Override
---
+++ src/main/java/r48/io/r2k/obj/ldb/EnemyAction.java
@@ -0,0 +1,60 @@
+/*
+ * This is released into the public domain.
+ * No warranty is provided, implied or otherwise.
+ */
+package r48.io.r2k.obj.ldb;
+
+import r48.RubyIO;
+import r48.io.r2k.Index;
+import r48.io.r2k.chunks.BooleanR2kStruct;
+import r48.io.r2k.chunks.IntegerR2kStruct;
+import r48.io.r2k.chunks.R2kObject;
+
+/**
+ * Created on 06/06/17.
+ */
+public class EnemyAction extends R2kObject {
+    public IntegerR2kStruct kind = new IntegerR2kStruct(0);
+    public IntegerR2kStruct basic = new IntegerR2kStruct(1);
+    public IntegerR2kStruct skillId = new IntegerR2kStruct(1);
+    public IntegerR2kStruct enemyId = new IntegerR2kStruct(1);
+    public IntegerR2kStruct conditionType = new IntegerR2kStruct(0);
+    public IntegerR2kStruct conditionP1 = new IntegerR2kStruct(0);
+    public IntegerR2kStruct conditionP2 = new IntegerR2kStruct(0);
+    public IntegerR2kStruct switchId = new IntegerR2kStruct(1);
+    public BooleanR2kStruct switchOn = new BooleanR2kStruct(false);
+    public IntegerR2kStruct switchOnId = new IntegerR2kStruct(1);
+    public BooleanR2kStruct switchOff = new BooleanR2kStruct(false);
+    public IntegerR2kStruct switchOffId = new IntegerR2kStruct(1);
+    public IntegerR2kStruct rating = new IntegerR2kStruct(50);
+    @Override
+    public Index[] getIndices() {
+        return new Index[] {
+                new Index(0x01, kind, "@act_kind"),
+                new Index(0x02, basic, "@act_basic"),
+                new Index(0x03, skillId, "@act_skill"),
+                new Index(0x04, enemyId, "@act_transform_enemy"),
+                new Index(0x05, conditionType, "@condition_type"),
+                new Index(0x06, conditionP1, "@condition_range_low"),
+                new Index(0x07, conditionP2, "@condition_range_high"),
+                new Index(0x08, switchId, "@condition_opt_switch_id"),
+                new Index(0x09, switchOn, "@act_set_switch"),
+                new Index(0x0A, switchOnId, "@act_set_switch_id"),
+                new Index(0x0B, switchOff, "@act_reset_switch"),
+                new Index(0x0C, switchOffId, "@act_reset_switch_id"),
+                new Index(0x0D, rating, "@rating"),
+        };
+    }
+
+    @Override
+    public RubyIO asRIO() {
+        RubyIO rio = new RubyIO().setSymlike("RPG::EnemyAction", true);
+        asRIOISF(rio);
+        return rio;
+    }
+
+    @Override
+    public void fromRIO(RubyIO src) {
+        fromRIOISF(src);
+    }
+}
