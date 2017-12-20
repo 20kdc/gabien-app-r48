c9a3052625361ecbbe67d43da784bcacf6fd49a4
20kdc <asdd2808@gmail.com>
2017-12-20 00:58:31 +0000

    Fixing all missing license headers

--- src/main/java/r48/io/r2k/obj/lsd/SaveMapInfo.java
+++ src/main/java/r48/io/r2k/obj/lsd/SaveMapInfo.java
@@ -1,3 +1,10 @@
+/*
+ * gabien-app-r48 - Editing program for various formats
+ * Written starting in 2016 by contributors (see CREDITS.txt)
+ * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+ * You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
 package r48.io.r2k.obj.lsd;
 
 import gabien.ui.ISupplier;
