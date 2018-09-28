32955325a1217df4224638b0aa39f6380caa434b
20kdc <asdd2808@gmail.com>
2018-09-28 15:50:05 +0100

    Hopefully fix the water AutoTileRules, and for that matter try to document AutoTileRules

--- src/main/java/r48/dbs/ATDB.java
+++ src/main/java/r48/dbs/ATDB.java
@@ -7,7 +7,7 @@
 
 package r48.dbs;
 
-import java.io.IOException;
+import java.util.LinkedList;
 
 /**
  * Autotile Database, used to deal with those pesky AutoTile issues
@@ -52,7 +52,7 @@ public class ATDB {
         return 0;
     }
 
-    public ATDB(String file) throws IOException {
+    public ATDB(String file) {
         loadFile = file;
         DBLoader.readFile(file, new IDatabase() {
             Autotile current = null;
@@ -95,98 +95,10 @@ public class ATDB {
         });
     }
 
-    public void calculateInverseMap(String file) throws IOException {
+    public void calculateInverseMap(String file) {
         if (file.equals("$WallATs$")) {
-            /*
-            // The Specific Case Symmetrical Combinator
-            // 124
-            // 1 2
-            // 412
-
-            // basic rules
-            // U
-            inverseMap[0370] = 2;
-            // UL corner
-            inverseMap[0320] = 3;
-            inverseMap[0321] = 3;
-            // ML
-            inverseMap[0326] = 1;
-            // LL corner
-            inverseMap[0026] = 9;
-            inverseMap[0066] = 9;
-            // L
-            inverseMap[0037] = 8;
-
-            // all cases of O
-            for (int i = 0; i < 16; i++) {
-                int idx = (i & 1) | ((i & 2) << 1) | ((i & 4) << 3) | ((i & 8) << 4);
-                inverseMap[idx] = 15;
-            }
-
-            // from observation file 1
-            // RTL, inside that then BTT
-            inverseMap[0050] = 6;
-
-            inverseMap[0010] = 14;
-            inverseMap[0017] = 8;
-            inverseMap[0170] = 2;
-
-            inverseMap[0031] = 10;
-            inverseMap[0076] = 9;
-            inverseMap[0360] = 3;
-
-            inverseMap[0022] = 13;
-            inverseMap[0306] = 5;
-            inverseMap[0124] = 7;
-
-            // Symmetrics
-            for (int i = 0; i < 256; i++) {
-                if (inverseMap[i] != 0) {
-                    boolean ul = (i & 1) != 0;
-                    boolean um = (i & 2) != 0;
-                    boolean ur = (i & 4) != 0;
-                    boolean ml = (i & 8) != 0;
-                    boolean mr = (i & 16) != 0;
-                    boolean ll = (i & 32) != 0;
-                    boolean lm = (i & 64) != 0;
-                    boolean lr = (i & 128) != 0;
-                    int flip = 0;
-                    if (ul)
-                        flip |= 4; // UR
-                    if (um)
-                        flip |= 2; // UM
-                    if (ur)
-                        flip |= 1; // UL
-
-                    if (ml)
-                        flip |= 16; // MR
-                    if (mr)
-                        flip |= 8; // ML
-
-                    if (ll)
-                        flip |= 128; // LR
-                    if (lm)
-                        flip |= 64; // LM
-                    if (lr)
-                        flip |= 32; // LL
-
-                    boolean vl = (inverseMap[i] & 1) != 0;
-                    boolean vr = (inverseMap[i] & 4) != 0;
-                    int flipVal = inverseMap[i] & 10;
-                    if (vl)
-                        flipVal |= 4; // VR
-                    if (vr)
-                        flipVal |= 1; // VL
-
-                    if (inverseMap[flip] != 0) {
-                        if (inverseMap[flip] != flipVal)
-                            throw new RuntimeException("ATDB WallAT hax inconsistent symm.");
-                    } else {
-                        inverseMap[flip] = flipVal;
-                    }
-                }
-            }*/
-            // The "WRONG BUT IT WORKS OKAY" ruleset
+            // This is wrong, but it works okayish.
+            // I haven't been able to get full information on this.
             for (int i = 0; i < 256; i++) {
                 // Tried and failed to get this consistent with the image Ozzy gave me for:
                 //  ###
@@ -214,7 +126,8 @@ public class ATDB {
         calculateInverseMapRulesEngine(file);
     }
 
-    public void calculateInverseMapRulesEngine(String file) throws IOException {
+    public void calculateInverseMapRulesEngine(String file) {
+        final LinkedList<Integer> avoidThese = new LinkedList<Integer>();
         DBLoader.readFile(file, new IDatabase() {
             @Override
             public void newObj(int objId, String objName) {
@@ -239,10 +152,13 @@ public class ATDB {
 
             @Override
             public void execCmd(char c, String[] args) {
-
+                if (c == 'C')
+                    if (args[0].equals("disable"))
+                        for (int i = 1; i < args.length; i++)
+                            avoidThese.add(Integer.parseInt(args[i]));
             }
         });
-        int issues = 0;
+        LinkedList<String> issues = new LinkedList<String>();
         for (int i = 0; i < 256; i++) {
             boolean[] area = new boolean[8];
             area[0] = (i & 1) != 0;
@@ -253,30 +169,26 @@ public class ATDB {
             area[5] = (i & 32) != 0;
             area[6] = (i & 64) != 0;
             area[7] = (i & 128) != 0;
-            inverseMap[i] = getMostSuitableAutotile(area);
-            if (inverseMap[i] == -1) {
-                String nano = "";
-                for (int j = 0; j < area.length; j++) {
-                    nano += area[j] ? "1" : "0";
-                }
-                System.out.println("ATR: " + nano);
-                issues++;
-                inverseMap[i] = 47;
-            }
+            inverseMap[i] = getMostSuitableAutotile(area, avoidThese, issues);
         }
-        if (issues > 0) {
+        if (issues.size() > 0) {
+            for (String s : issues)
+                System.out.println("ATR: " + s);
             System.out.println("     NNNWESSS");
             System.out.println("     W E  W E");
-            System.out.println("There are " + issues + " situations in which placing " + loadFile + " ATs leads to an ambiguous result.");
+            System.out.println("There are " + issues.size() + " situations in which placing " + loadFile + " ATs leads to an ambiguous result.");
             System.out.println("(In the default RXP dataset, 16 situations is normal, and the default value of AT47 will work.)");
         }
     }
 
-    private int getMostSuitableAutotile(boolean[] area) {
-        int bestAT = 47;
+    private int getMostSuitableAutotile(boolean[] area, LinkedList<Integer> avoidThese, LinkedList<String> issues) {
+        int bestAT = 0;
         int bestATReq = 0;
         boolean multiplePossible = true;
-        for (int i = 0; i < 48; i++) {
+        StringBuilder issueResolution = new StringBuilder(" NONE");
+        for (int i = 0; i < entries.length; i++) {
+            if (avoidThese.indexOf(i) != -1)
+                continue;
             if (entries[i] != null) {
                 int reqCount = 0;
                 boolean fail = false;
@@ -308,20 +220,24 @@ public class ATDB {
                 if (reqCount > bestATReq) {
                     bestAT = i;
                     bestATReq = reqCount;
+                    issueResolution = new StringBuilder(" " + Integer.toString(i));
                     multiplePossible = false;
                 } else if (reqCount == bestATReq) {
+                    issueResolution.append(' ');
+                    issueResolution.append(Integer.toString(i));
                     multiplePossible = true;
                 }
             }
         }
         if (multiplePossible) {
-            // It seems 47 is actually the correct response in some cases.
-            /*
-            String s = "";
-            for (int k = 0; k < 8; k++)
-                s += area[k] ? "1" : "0";
-            System.out.println("Situation code " + s);*/
-            return -1;
+            // Alert the user to the ambiguous result but still return first as best.
+            if (issues != null) {
+                String nano = "";
+                for (int j = 0; j < area.length; j++)
+                    nano += area[j] ? "1" : "0";
+                issues.add(nano + issueResolution.toString());
+            }
+            return bestAT;
         }
         return bestAT;
     }
@@ -331,174 +247,6 @@ public class ATDB {
         for (int i = 0; i < 8; i++) {
             mustTrue[i] |= rulesEngineMustTrue[base + i];
             mustFalse[i] |= rulesEngineMustFalse[base + i];
-        }
-    }
-    // this is basically guesswork
-
-    private void setMasksForCornerOLD(boolean[] mustTrue, boolean[] mustFalse, int corner, int ci) {
-        switch (corner) {
-            case 0:
-                switch (ci) {
-                    case 0:
-                        mustFalse[1] = true;
-                        mustFalse[3] = true;
-                        break;
-                    case 1:
-                        mustFalse[1] = true;
-                        mustFalse[4] = true;
-                        break;
-                    case 2:
-                        mustFalse[3] = true;
-                        mustFalse[6] = true;
-                        break;
-                    case 3:
-                        mustFalse[4] = true;
-                        mustFalse[6] = true;
-                        break;
-                }
-                break;
-            case 2:
-                switch (ci) {
-                    case 0:
-                        mustTrue[1] = true;
-                        mustFalse[0] = true;
-                        mustTrue[3] = true;
-                        break;
-                    case 1:
-                        mustTrue[1] = true;
-                        mustFalse[2] = true;
-                        mustTrue[4] = true;
-                        break;
-                    case 2:
-                        mustTrue[3] = true;
-                        mustFalse[5] = true;
-                        mustTrue[6] = true;
-                        break;
-                    case 3:
-                        mustTrue[4] = true;
-                        mustFalse[7] = true;
-                        mustTrue[6] = true;
-                        break;
-                }
-                break;
-            case 7:
-                switch (ci) {
-                    case 0:
-                        mustTrue[1] = true;
-                        mustTrue[0] = true;
-                        mustTrue[3] = true;
-                        break;
-                    case 1:
-                        mustTrue[1] = true;
-                        mustTrue[2] = true;
-                        mustTrue[4] = true;
-                        break;
-                    case 2:
-                        mustTrue[3] = true;
-                        mustTrue[5] = true;
-                        mustTrue[6] = true;
-                        break;
-                    case 3:
-                        mustTrue[4] = true;
-                        mustTrue[7] = true;
-                        mustTrue[6] = true;
-                        break;
-                }
-                break;
-
-            // 012
-            // 3 4
-            // 567
-            //Cr Cut.
-            // 0.2
-            // 345
-            // 678
-            // 9**
-            // 01
-            // 23
-            case 3:
-                if (ci == 0) {
-                    mustFalse[1] = true;
-                    mustFalse[3] = true;
-                } else if (ci == 3) {
-                    // using the UL tile on 3 means this is a full UL
-                    mustTrue[4] = true;
-                    mustTrue[7] = true;
-                    mustTrue[6] = true;
-                }
-                break;
-            case 4:
-                if ((ci == 0) || (ci == 1)) {
-                    mustFalse[1] = true;
-
-                    mustTrue[3] |= ci == 0;
-                    mustTrue[4] |= ci == 1;
-                }
-                break;
-            case 5:
-                if (ci == 1) {
-                    mustFalse[1] = true;
-                    mustFalse[4] = true;
-                } else if (ci == 2) {
-                    mustTrue[3] = true;
-                    mustTrue[5] = true;
-                    mustTrue[6] = true;
-                }
-                break;
-            case 6:
-                if ((ci == 0) || (ci == 2)) {
-                    mustFalse[3] = true;
-
-                    mustTrue[1] |= ci == 0;
-                    mustTrue[6] |= ci == 2;
-                }
-                break;
-            // 012
-            // 3 4
-            // 567
-            //Cr Cut.
-            // 0.2
-            // 345
-            // 678
-            // 9**
-            // 01
-            // 23
-            case 8:
-                if ((ci == 1) || (ci == 3)) {
-                    mustFalse[4] = true;
-
-                    mustTrue[1] |= ci == 1;
-                    mustTrue[6] |= ci == 3;
-                }
-                break;
-            case 9:
-                if (ci == 2) {
-                    mustFalse[3] = true;
-                    mustFalse[6] = true;
-                } else if (ci == 1) {
-                    mustTrue[1] = true;
-                    mustTrue[2] = true;
-                    mustTrue[4] = true;
-                }
-                break;
-            case 10:
-                if ((ci == 2) || (ci == 3)) {
-                    mustFalse[6] = true;
-
-                    mustTrue[3] |= ci == 2;
-                    mustTrue[4] |= ci == 3;
-                }
-                break;
-            case 11:
-                if (ci == 3) {
-                    mustFalse[4] = true;
-                    mustFalse[6] = true;
-                } else if (ci == 0) {
-                    mustTrue[3] = true;
-                    mustTrue[0] = true;
-                    mustTrue[1] = true;
-                }
-                break;
         }
     }
 
