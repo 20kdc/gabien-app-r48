967a42357353ea0c9b19b4924edea155d48f3d4d
20kdc <asdd2808@gmail.com>
2023-07-24 11:34:47 +0100

    Fix a missing license header.
    I know, boring...

--- app/src/main/java/r48/map/tileedit/TileEditingTab.java
+++ app/src/main/java/r48/map/tileedit/TileEditingTab.java
@@ -1,3 +1,10 @@
+/*
+ * gabien-app-r48 - Editing program for various formats
+ * Written starting in 2016 by contributors (see CREDITS.txt)
+ * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+ * A copy of the Unlicense should have been supplied as COPYING.txt in this repository. Alternatively, you can find it at <https://unlicense.org/>.
+ */
+
 package r48.map.tileedit;
 
 import r48.App;
