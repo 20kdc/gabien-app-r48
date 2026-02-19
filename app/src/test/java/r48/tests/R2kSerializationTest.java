3f96ed2abb567bb454159bf6bc812fc777b00f7d
t20kdc <asdd2808@gmail.com>
2026-02-19 22:20:03 +0000

    Add test to try and instance all R2k structs, close #69

--- app/src/test/java/r48/tests/R2kSerializationTest.java
+++ app/src/test/java/r48/tests/R2kSerializationTest.java
@@ -13,9 +13,22 @@ import org.junit.Test;
 
 import r48.R48;
 import r48.dbs.ObjectRootHandle;
+import r48.io.data.DMContext;
+import r48.io.data.DMKey;
+import r48.io.data.IRIO;
+import r48.io.data.obj.FixedObjectProps;
+import r48.io.data.obj.IRIOFixedObject;
+import r48.io.r2k.chunks.IR2kInterpretable;
+import r48.io.r2k.obj.MapUnit;
+import r48.io.r2k.obj.Save;
+import r48.io.r2k.obj.ldb.Database;
+import r48.io.r2k.struct.MapTree;
+import r48.io.undoredo.DMChangeTracker;
 import r48.ioplus.Reporter;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created on December 02, 2018.
@@ -45,5 +58,46 @@ public class R2kSerializationTest {
             ObjectRootHandle i = app.odb.getObject(s, false);
             Assert.assertNotNull(i);
         }
+    }
+    @Test
+    public void testCreateAllReachableStructs() throws IOException {
+        DMContext dmc = new DMContext(DMChangeTracker.Null.TESTS, StandardCharsets.UTF_8);
+        Save save = new Save(dmc);
+        MapUnit mu = new MapUnit(dmc);
+        MapTree mt = new MapTree(dmc);
+        Database db = new Database(dmc);
+        createAllReachableStructsAndSave(save);
+        createAllReachableStructsAndSave(mu);
+        createAllReachableStructsAndSave(mt);
+        createAllReachableStructsAndSave(db);
+    }
+
+    public void createAllReachableStructsAndSave(IR2kInterpretable obj) {
+        createAllReachableStructs((IRIO) obj);
+        try {
+            obj.exportData(new ByteArrayOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void createAllReachableStructs(IRIO obj) {
+        if (obj instanceof IRIOFixedObject) {
+            IRIOFixedObject ifo = (IRIOFixedObject) obj;
+            FixedObjectProps fops = new FixedObjectProps(ifo.getClass());
+            for (FixedObjectProps.FXOBinding fb : fops.fxoBindingsArray)
+                if (fb.optional)
+                    ifo.addIVar(fb.iVar);
+        }
+        if (obj.getType() == '[') {
+            while (obj.getALen() < 2)
+                obj.addAElem(obj.getALen());
+            for (int i = 0; i < obj.getALen(); i++)
+                createAllReachableStructs(obj.getAElem(i));
+        } else if (obj.getType() == '{' || obj.getType() == '}') {
+            createAllReachableStructs(obj.addHashVal(DMKey.of(1)));
+        }
+        for (String s : obj.getIVars())
+            createAllReachableStructs(obj.getIVar(s));
     }
 }
